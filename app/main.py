f4fa3183b79ed22ceeb13f6f59ac690444eb611b
Yahor Harunovich <pixel.egor@gmail.com>
2024-05-22 23:59:03 +0200

    pass stage

--- app/main.py
+++ app/main.py
@@ -4,8 +4,9 @@ import socket
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     client_socket, _ = server_socket.accept()
-    client_socket.recv(1024)
-    client_socket.sendall(b"+PONG\r\n")
+    while True:
+        client_socket.recv(1024)
+        client_socket.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
