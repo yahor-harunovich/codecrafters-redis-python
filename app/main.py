912b1901df978e0cd29f86146b9223177d2ec3b2
Yahor Harunovich <pixel.egor@gmail.com>
2024-05-22 23:46:14 +0200

    run tests

--- app/main.py
+++ app/main.py
@@ -3,11 +3,9 @@ import socket
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, address = server_socket.accept() # wait for client
-    with connection:
-        connection.recv(1024)
-        response = "+PONG\r\n"
-        connection.sendall(response.encode("utf-8")) 
+    client_socket, _ = server_socket.accept()
+    client_socket.recv(1024)
+    client_socket.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
