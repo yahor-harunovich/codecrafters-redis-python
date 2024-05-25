1e2300462d5cb872bb205a8b74fbe96a3bbca2d9
Yahor Harunovich <pixel.egor@gmail.com>
2024-05-25 12:46:51 +0200

    Add error handling

--- app/main.py
+++ app/main.py
@@ -1,23 +1,47 @@
+from concurrent.futures import ThreadPoolExecutor
+import logging
 import socket
-import threading
+
+
+SERVER_ADDRESS = ("localhost", 6379)
+MAX_WORKERS = 50
+BUFFER_SIZE = 1024
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 
 def handle_client(client_socket: socket.socket) -> None:
 
-    while True:
-        client_socket.recv(1024)
-        client_socket.sendall(b"+PONG\r\n")
+    try:
+        with client_socket:
+            while True:
+                data = client_socket.recv(BUFFER_SIZE)
+                logging.info(f"Received from {client_socket.getpeername()}: {data}") 
+                if not data:
+                    break
+                client_socket.sendall(b"+PONG\r\n")
+    except Exception as e:
+        logging.error(f"Error: {e}")
 
+    logging.info(f"Connection from {client_socket.getpeername()} closed")
 
 def main() -> None:
 
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    try:
+        with socket.create_server(SERVER_ADDRESS, reuse_port=True) as server_socket:
+            logging.info(f"Server listening on {SERVER_ADDRESS}")
+            with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+                while True:
+                    client_socket, _ = server_socket.accept()
+                    logging.info(f"Accepted connection from {client_socket.getpeername()}")
+                    executor.submit(handle_client, client_socket) 
 
-    while True:
-        client_socket, _ = server_socket.accept()
-        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
-        client_thread.start()
-    
+    except Exception as e:
+        logging.error(f"Error: {e}")
+        
+    logging.info("Server stopped") 
+
 
 if __name__ == "__main__":
     main()
