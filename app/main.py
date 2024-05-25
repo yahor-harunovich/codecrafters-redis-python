b064e2d0cf11c872a1a619016ec833f9bdd9cc39
Yahor Harunovich <pixel.egor@gmail.com>
2024-05-25 12:15:18 +0200

    handle concurrent clients

--- app/main.py
+++ app/main.py
@@ -1,13 +1,23 @@
 import socket
+import threading
 
 
-def main():
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client_socket, _ = server_socket.accept()
+def handle_client(client_socket: socket.socket) -> None:
+
     while True:
         client_socket.recv(1024)
         client_socket.sendall(b"+PONG\r\n")
 
+
+def main() -> None:
+
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+
+    while True:
+        client_socket, _ = server_socket.accept()
+        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+        client_thread.start()
+    
 
 if __name__ == "__main__":
     main()
